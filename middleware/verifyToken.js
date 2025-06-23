48cd86576a5221df79d90beb2cc08910ec30ed7e
Mahesh Talluri <mtalluri563@gmail.com>
2025-06-23 19:16:31 +0530

    added

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,43 +1,4 @@
 
-// import jwt from "jsonwebtoken";
-
-// // ✅ Verify JWT Token
-// export const verifyToken = (req, res, next) => {
-//   const token =
-//     req.cookies?.accessToken || req.headers.authorization?.split(" ")[1];
-
-//   console.log(
-//     "🔹 Verifying token:",
-//     token ? `Token exists: ${token}` : "No token"
-//   );
-
-//   if (!token) {
-//     console.error("❌ No token provided");
-//     return res.status(401).json({ error: "Not authenticated!" });
-//   }
-
-//   try {
-//     const userInfo = jwt.verify(token, process.env.JWT_SECRET);
-//     console.log(
-//       "✅ Token verified for user:",
-//       userInfo.id,
-//       "Token Expires At:",
-//       new Date(userInfo.exp * 1000)
-//     );
-
-//     req.userInfo = userInfo; // ✅ Add user info to request
-//     next(); // Proceed to controller
-//   } catch (err) {
-//     if (err.name === "TokenExpiredError") {
-//       console.error("❌ Token has expired!");
-//       return res.status(403).json({ error: "Token expired. Please login again." });
-//     }
-//     console.error("❌ Token verification failed:", err.message);
-//     return res.status(403).json({ error: "Token is not valid!" });
-//   }
-// };
-
-
 
 // middleware/verifyToken.js
 
